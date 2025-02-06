375566e8d42e983f553cfedecd2bef007e52331e
Mrunal Vekariya <mrunalvekariya@MacBook-Air.local>
2025-02-05 20:52:55 -0500

    first commit

--- src/Kambaz/index.tsx
+++ src/Kambaz/index.tsx
@@ -1,4 +1,3 @@
-import "./styles.css";
 import { Routes, Route, Navigate }
   from "react-router";
 import Account from "./Account";
@@ -8,17 +7,23 @@ import Courses from "./Courses";
 export default function Kambaz() {
   return (
     <div id="wd-kambaz">
-              <KambazNavigation />
-            <div className="wd-main-content-offset p-3">
-              <Routes>
-                <Route path="/" element={<Navigate to="/Kambaz/Account" />} />
-                <Route path="/Account/*" element={<Account />} />
-                <Route path="/Dashboard" element={<Dashboard />} />
-                <Route path="/Courses/:cid/*" element={<Courses />} />
-                <Route path="/Calendar" element={<h1>Calendar</h1>} />
-                <Route path="/Inbox" element={<h1>Inbox</h1>} />
-              </Routes>
-            </div>
+      <table>
+        <tr>
+          <td valign="top">
+            <KambazNavigation />
+          </td>
+          <td valign="top">
+            <Routes>
+              <Route path="/" element={<Navigate to="Account" />} />
+              <Route path="/Account/*" element={<Account />} />
+              <Route path="/Dashboard" element={<Dashboard />} />
+              <Route path="/Courses/:cid/*" element={<Courses />} />
+              <Route path="/Calendar" element={<h1>Calendar</h1>} />
+              <Route path="/Inbox" element={<h1>Inbox</h1>} />
+            </Routes>
+          </td>
+        </tr>
+      </table>
     </div>
   );
 }
